78c10491eb0932235ced2065b2b9dcc923c9e6dc
King Kong <king-kong@gmail.com>
2022-09-07 16:40:19 -0700

    added name input and change name.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // import functions and grab DOM elements
-
+const nameEl = document.getElementById('name-input');
+const changeButton = document.getElementById('change-name');
+console.log(nameEl);
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+changeButton.addEventListener('click', () => {});
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
