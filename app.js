75c31b5162f600a52140807e4236f4bcaddc7e45
King Kong <king-kong@gmail.com>
2022-09-09 12:45:25 -0700

    name tag is working might add some styling later

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // import functions and grab DOM elements
 const nameEl = document.getElementById('name-input');
 const changeButton = document.getElementById('change-name');
+const myName = document.getElementById('name-display');
 console.log(nameEl);
 // let state
 
 // set event listeners
-changeButton.addEventListener('click', () => {});
+changeButton.addEventListener('click', () => {
+    const value = nameEl.value;
+    myName.textContent = value;
+});
+
 // get user input
 // use user input to update state
 // update DOM to reflect the new state
